36d221d2791a693e7b4f8d5bcb7ba4970b6744fc
Ali Shareeff <shareefa@mcmaster.ca>
2025-03-22 14:04:26 -0400

    just comitting minor changes

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SearchAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SearchAlgorithm.java
@@ -1,8 +1,8 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
-import org.json.JSONObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 
 public class SearchAlgorithm {
     private final Logger logger = LogManager.getLogger();
