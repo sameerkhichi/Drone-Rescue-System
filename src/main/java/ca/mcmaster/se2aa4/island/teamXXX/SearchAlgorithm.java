a0daf41284c5397a4279a2a5f57e2607c57851df
Sameer <sameerrkhichi@gmail.com>
2025-03-24 07:40:00 -0400

    Refactoring design to introduce abstractions and improve scalability.

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SearchAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/SearchAlgorithm.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
-public class SearchAlgorithm {
+public class SearchAlgorithm implements SearchStrategy{
     private final Logger logger = LogManager.getLogger();
 
     private DroneState drone;
